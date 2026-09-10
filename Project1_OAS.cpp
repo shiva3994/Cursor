a718e9c8b25f4fad5f30cc1cc9bf229b6a132a6a
Shiva Thakur <63214655+shiva3994@users.noreply.github.com>
2026-09-11 01:08:16 +0530

    Create Project1_OAS.cpp

---
+++ Project1_OAS.cpp
@@ -0,0 +1,14 @@
+// Project 1: Obstacle-Avoidance Simulator
+
+// What it does: simulates a robot checking 5 sensors, printing each reading's status,
+// then giving one final decision.
+
+// Part A — set up the array (you know this):
+
+#include <iostream> 
+int main() {
+    int sensor_readings[5] = {12, 8, 15, 3, 20};
+    retutn 0;
+}
+
+//
