773c269dc8bc5f6a7d575142b2bace689a52ec19
Shiva Thakur <63214655+shiva3994@users.noreply.github.com>
2026-09-12 00:30:49 +0530

    Update Project2_Functions.cpp

--- Project2_Functions.cpp
+++ Project2_Functions.cpp
@@ -29,3 +29,5 @@ int main() {                                            // program starts here
     
         return 0;                                           // program finished successfully
     }
+
+    g++ Project2_Functions.cpp -o Project2_Functions.exe; .\Project2_Functions.exe
