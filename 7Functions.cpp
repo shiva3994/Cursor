fa9ff3ae14388f5a2b32783df7866421800fe9db
Shiva Thakur <63214655+shiva3994@users.noreply.github.com>
2026-09-12 00:08:57 +0530

    U

--- 7Functions.cpp
+++ 7Functions.cpp
@@ -1,9 +1,17 @@
+#include <iostream>
+
 void check_sensor(int distance) {
     if (distance < 10) {
         std::cout << "DANGER" << std::endl;
     } else {
         std::cout << "clear" << std::endl;
     }
+}
+
+int main() {
+    check_sensor(5);
+    check_sensor(20);
+    return 0;
 }
 
 // g++ 7Functions.cpp -o 7Functions.exe; .\7Functions.exe
