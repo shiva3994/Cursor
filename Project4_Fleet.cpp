a95cee7d99156704df93a1424ee5775877a38581
Shiva Thakur <63214655+shiva3994@users.noreply.github.com>
2026-09-15 05:01:06 +0530

    U

--- Project4_Fleet.cpp
+++ Project4_Fleet.cpp
@@ -13,30 +13,29 @@ void reportStatus(Robot r) {                  // function takes a whole Robot as
     std::cout << "Position: " << r.position << std::endl;              // print position
 }                                              // end of reportStatus function
 
-int main() {                                  // program starts here
-    Robot fleet[3];                           // array holding 3 Robots
+int main() {                                            // program starts here
+    Robot fleet[3];                                     // array holding 3 Robots
 
-    fleet[0].name = 'Rover0';
-    fleet[0].battery_level = 10;
-    fleet[0].position = 0;
+    fleet[0].name = "Rover 0";                           // set 1st robot's name
+    fleet[0].battery_level = 10;                        // set 1st robot's battery (low, for testing warning)
+    fleet[0].position = 0;                              // set 1st robot's position
 
-    fleet[1].name = 'Rover1';
-    fleet[1].battery_level = 90;
-    fleet[1].position = 0;
+    fleet[1].name = "Rover 1";                           // set 2nd robot's name
+    fleet[1].battery_level = 90;                        // set 2nd robot's battery
+    fleet[1].position = 0;                              // set 2nd robot's position
 
-    fleet[2].name = 'Rover2';
-    fleet[2].battery_level = 88;
-    fleet[2].position = 0;
+    fleet[2].name = "Rover 2";                           // set 3rd robot's name
+    fleet[2].battery_level = 88;                        // set 3rd robot's battery
+    fleet[2].position = 0;                              // set 3rd robot's position
 
-    std::cout << fleet[0].name << ": " << fleet[0].battery_level << "%" << std::endl;  // print 1st robot
-    std::cout << fleet[1].name << ": " << fleet[1].battery_level << "%" << std::endl;  // print 2nd robot
-    std::cout << fleet[2].name << ": " << fleet[2].battery_level << "%" << std::endl;  // print 3rd robot
+    for (int i = 0; i < 3; i++) {                       // loop through all 3 robots (index 0 to 2)
+        reportStatus(fleet[i]);                         // print this robot's full status using the function
+        if (fleet[i].battery_level < 30) {              // check if this robot's battery is low
+            std::cout << "WARNING: " << fleet[i].name << " needs charging!" << std::endl;  // print warning
+        }
+    }                                                    // loop ends after checking all 3 robots
 
-    // TODO: use a for loop (i = 0 to 2) to call reportStatus(fleet[i])
-    //       and print a warning if fleet[i].battery_level < 30
-
-    return 0;                                 // program finished successfully
+    return 0;                                           // program finished successfully
 }
-
 
 //  g++ Project4_Fleet.cpp -o Project4_Fleet.exe; .\Project4_Fleet.exe
